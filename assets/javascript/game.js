fefa17a78fd02de56e7d9d540ec52276bb5f12f0
wan03 <38962452+wan03@users.noreply.github.com>
2018-09-04 20:48:37 -0400

    Pseudo coded js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// on load function.
+
+
+// declare needed variables.
+
+
+// create computer randomly generated number between 19 - 120.
+
+
+
+// Create win and losses counter.
+
+
+
+// Create crystals logic.
+
+
+// Keep total score from crystals.
+
+
+
+// Determine win/lose condition (may be placed somewhere else).
